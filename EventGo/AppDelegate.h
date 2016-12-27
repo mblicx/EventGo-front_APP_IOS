5b11aff108b504e56a0bf56075a827f7e0ba863e
ZhangXulong <zhangxulong@zhangxuongdembp>
2016-12-27 16:47:59 +0100

    Initial Commit

---
+++ EventGo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  EventGo
+//
+//  Created by ZhangXulong on 16/12/27.
+//  Copyright © 2016年 ShuopuLI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
